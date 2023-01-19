49763663f71f238641b3c08308362ecc42c0793a
Matt Payne <Payne@MattPayne.org>
2023-01-18 22:57:34 -0600

    Finally figured out how to trigger the update bits.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Injectable, Component, OnInit } from '@angular/core';
 import { Platform } from '@angular/cdk/platform';
 import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { filter, map } from 'rxjs/operators';
+import { concat, interval } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -16,18 +18,39 @@ export class AppComponent implements OnInit {
   modalPwaPlatform: string|undefined;
 
   constructor(private platform: Platform,
+              private appRef: ApplicationRef,
               private swUpdate: SwUpdate) {
+    console.log(`Trying to trigger update 2.`);
     this.isOnline = false;
     this.modalVersion = false;
-  }
+ }
 
   public ngOnInit(): void {
     this.updateOnlineStatus();
 
     window.addEventListener('online',  this.updateOnlineStatus.bind(this));
     window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+    // From: https://angular.io/guide/service-worker-communications
+    // Allow the app to stabilize first, before starting
+    // polling for updates with `interval()`.
+    const appIsStable$ = this.appRef.isStable.pipe(first(isStable => isStable === true));
+    // const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHours$ = interval(40 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+    everySixHoursOnceAppIsStable$.subscribe(async () => {
+      try {
+        console.log(`About to check for new version.`);
+        const updateFound = await this.swUpdate.checkForUpdate();
+        console.log(updateFound ? 'A new version is available.' : 'Already on the latest version.');
+        if (updateFound) this.modalVersion = true;
+      } catch (err) {
+        console.error('Failed to check for updates:', err);
+      }
+    });
 
     if (this.swUpdate.isEnabled) {
+      console.log(`swUpdate is enabled.`);
       this.swUpdate.versionUpdates.pipe(
         filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
         map((evt: any) => {
@@ -35,7 +58,21 @@ export class AppComponent implements OnInit {
           this.modalVersion = true;
         }),
       );
+    } else {
+      console.log(`swUpdate is NOT enabled.`);
     }
+
+    /*
+    this.swUpdate.checkForUpdate().then(() => {
+      console.info('checkForUpdate');
+    })
+      .catch((err: any) => {
+        console.error('checkForUpdate', err);
+      })
+      .finally(() => {
+      console.log('checkForUpdate finally');
+    });
+     */
 
     this.loadModalPwa();
   }
